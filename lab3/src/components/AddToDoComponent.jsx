e7e9451e6d98c436581e9473bebba4e16b9c0496
NoskoTaras <taras.nosko@oa.edu.ua>
2024-09-19 21:28:58 +0300

    Iniit: Lab3 comp
    :)

---
+++ lab3/src/components/AddToDoComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AddToDOComponent = ({ title, onTitleChange, onSubmit }) => {
+  return (
+    <form className="add-todo-form" onSubmit={onSubmit}>
+      <input
+        type="text"
+        value={title || ""}
+        onChange={onTitleChange}
+        placeholder="New ToDo"
+      />
+      <button className="add-todo-button" type="submit">
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default AddToDOComponent;
